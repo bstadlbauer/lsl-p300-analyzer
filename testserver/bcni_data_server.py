276e30fa54ef08eada5992dc80d59a5f94fa7ee4
Bernhard Stadlbauer <bernhard.stadlbauer@invenium.io>
2019-02-03 12:58:09 +0100

    refactor: Move testserver out

---
+++ testserver/bcni_data_server.py
@@ -0,0 +1,69 @@
+'''
+Created on Mar 26, 2017
+
+@author: bstad
+'''
+import multiprocessing as mp
+import time
+
+from pylsl import StreamInfo, StreamOutlet, IRREGULAR_RATE
+
+from testserver.bcnidata import BCNIData
+
+
+class BCNIDataServer(mp.Process):
+
+    def __init__(self, filename):
+        mp.Process.__init__(self)
+        self.data = BCNIData(filename)
+
+    def second_init(self):
+        '''Used because otherwise multiprocessing cannot pickle object
+        '''
+        # self.data.load_file()
+
+        # LSL Settings
+        self.eeg_streamname = 'testeeg_stream'
+        self.marker_streamname = 'marker_stream'
+        self.period_time = 1.0 / self.data.samplerate  # seconds
+
+        # EEG stream
+        self.eeg_info = StreamInfo(name=self.eeg_streamname, nominal_srate=self.data.samplerate,
+                                   type='EEG', channel_count=self.data.num_channels, channel_format='float32',
+                                   source_id='eeg_stream_test', handle=None)
+        self.lsl_eeg = StreamOutlet(self.eeg_info)
+
+        # Marker Stream
+        self.marker_info = StreamInfo(name=self.marker_streamname, type='P300_Marker',
+                                      nominal_srate=IRREGULAR_RATE,
+                                      channel_count=1, channel_format='int8',
+                                      source_id='marker_stream', handle=None)
+        self.marker_info.desc().append_child_value('flash_mode', self.data.flash_mode)
+        self.marker_info.desc().append_child_value('num_rows', '6')
+        self.marker_info.desc().append_child_value('num_cols', '6')
+        self.lsl_marker = StreamOutlet(self.marker_info)
+
+    def run(self):
+        self.second_init()
+
+        # Stream data
+        print('Server started...')
+        print('EEG samples stream: \'' + self.eeg_streamname + '\'')
+        print('P300 Markers stream: \'' + self.marker_streamname + '\'')
+
+        # Hack to start later in the dataset
+        self.data.set_counter(3000)
+
+        while True:
+            timestamp = self.data.get_timestamp()
+            eeg_data = self.data.get_eeg_data()
+            marker = self.data.get_marker()
+            self.data.increase_counter()
+
+            self.lsl_eeg.push_sample(eeg_data, timestamp)
+
+            # Push only non zero markers
+            if marker[0] != 0:
+                self.lsl_marker.push_sample(marker, timestamp)
+
+            time.sleep(self.period_time)
---
+++ testserver/bcnidata.py
@@ -0,0 +1,80 @@
+'''
+Created on Apr 15, 2017
+
+@author: bstad
+'''
+import os
+
+from scipy import io
+
+
+class BCNIData(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, filename):
+        '''
+        Constructor
+        '''
+        self.filename = filename
+        self.counter = 0
+
+        self.timestamps = []
+        self.eeg_data = []
+        self.markers = []
+        self.target = []
+        self.length = None
+        self.flash_mode = 'Single Value'
+        self.samplerate = 256  # Hz
+        self.num_channels = None
+
+        self.load_file()
+
+    def get_timestamp(self):
+        return self.timestamps[self.counter % self.length]
+
+    def get_eeg_data(self):
+        return self.eeg_data[:, self.counter % self.length]
+
+    def get_marker(self):
+        return [int(self.markers[self.counter % self.length])]
+
+    def set_counter(self, value):
+        self.counter = value
+
+    def increase_counter(self):
+        self.counter += 1
+
+    def load_file(self):
+        self.file_location = os.path.dirname(__file__)
+        self.prefix = '/BNCI Horizon - Guger 2009/Data/'
+
+        self.path = self.file_location + self.prefix + self.filename
+
+        self.matrix = io.loadmat(self.path)['s1'][0][0][1]  # axis: [filename][0][0][train/test]
+        self.timestamps = self.matrix[0]
+        self.eeg_data = self.matrix[1:9]
+
+        self.markers = self.normalize_markers(self.matrix[9].astype(int))
+
+        self.target = self.normalize_markers(self.matrix[10].astype(int))
+        #         self.target = self.matrix[10].astype(int)
+
+        self.length = len(self.timestamps)
+        self.num_channels = len(self.eeg_data)
+
+    def normalize_markers(self, markers):
+        """ Removes additional markers if there is more than one 
+        """
+        init = 2
+        new_flash_ids = markers[0:init].tolist()
+        number_of_ids = len(markers)
+
+        for i in range(init, number_of_ids):
+            if markers[i] != 0 and markers[i - 1] == 0:
+                new_flash_ids.append(markers[i])
+            else:
+                new_flash_ids.append(0)
+
+        return new_flash_ids
