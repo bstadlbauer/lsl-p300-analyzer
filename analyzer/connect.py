16e1fe3785f9c205c3226493e309c87a8701265b
Bernhard Stadlbauer <bernhard.stadlbauer@invenium.io>
2019-02-03 11:47:12 +0100

    Cleanup

--- analyzer/connect.py
+++ analyzer/connect.py
@@ -27,10 +27,10 @@ class ConnectorProc(object):
         '''
         self.eeg_inlet = None
         self.marker_inlet = None
-        
+
         self.lsl_rec_threads = []
         self.analysis_thread = []
-        
+
         self.recorded_data = None
 
         manager = mp.Manager()
@@ -65,48 +65,49 @@ class ConnectorProc(object):
     def create_inlets(self):
         self.eeg_inlet = self.create_inlet(self.connector_dict["eeg streamname"])
         self.marker_inlet = self.create_inlet(self.connector_dict["marker streamname"])
-           
+
     def create_inlet(self, name):
         streams = resolve_stream("name", str(name))
         inlet = StreamInlet(streams[0])
 
         if len(streams) != 1:
             self.print_to_console("ATTENTION: More streams with name: \"" +
-                  name + "\" found, using the first one")
-              
+                                  name + "\" found, using the first one")
+
         return inlet
-    
+
     def update_lsl_metadata(self):
         info_eeg = self.eeg_inlet.info()
         self.connector_dict["number of channels"] = info_eeg.channel_count()
         self.connector_dict["samplerate"] = info_eeg.nominal_srate()
-        
+
         info_marker = self.marker_inlet.info()
         self.connector_dict["num rows"] = int(info_marker.desc().child_value("num_rows"))
         self.connector_dict["num cols"] = int(info_marker.desc().child_value("num_cols"))
         self.connector_dict["flash mode"] = info_marker.desc().child_value("flash_mode")
-        
+
     def print_to_console(self, message):
         self.message_q.put(message)
-        
+
     def run(self):
         self.ready_for_connection_e.wait()
         self.create_inlets()
         self.update_lsl_metadata()
-        
+
         samplerate = self.connector_dict["samplerate"]
         number_of_channels = self.connector_dict["number of channels"]
-        self.print_to_console("Connected! There are {} channels with a samplerate of {}Hz".format(number_of_channels, samplerate))
-        
+        self.print_to_console(
+            "Connected! There are {} channels with a samplerate of {}Hz".format(number_of_channels, samplerate))
+
         self.recorded_data = RecordedData(self.connector_dict["filter"])
         self.recorded_data.set_samplerate(samplerate)
         self.recorded_data.set_num_channel(number_of_channels)
-        
-        eeg_thread = LSLReceiverThread(self.eeg_inlet, self.recorded_data.append_eeg_sample, 
+
+        eeg_thread = LSLReceiverThread(self.eeg_inlet, self.recorded_data.append_eeg_sample,
                                        self.recorded_data.append_eeg_ts, self.connector_dict)
-        marker_thread = LSLReceiverThread(self.marker_inlet, self.recorded_data.append_marker_sample, 
+        marker_thread = LSLReceiverThread(self.marker_inlet, self.recorded_data.append_marker_sample,
                                           self.recorded_data.append_marker_ts)
-        
+
         self.lsl_rec_threads.append(eeg_thread)
         self.lsl_rec_threads.append(marker_thread)
 
@@ -114,9 +115,9 @@ class ConnectorProc(object):
         axis_to_plotter_queue = mp.Queue()
         self.analysis_thread = AnalysisThread(self.recorded_data, self.connector_dict, self.message_q,
                                               data_to_plotter_queue, axis_to_plotter_queue)
-        
+
         self.connected_e.set()
-        
+
         self.start_recording_e.wait()
 
         for thread in self.lsl_rec_threads:
@@ -128,7 +129,7 @@ class ConnectorProc(object):
         num_rows = self.connector_dict["num rows"]
         num_cols = self.connector_dict["num cols"]
         plotter = Plotter(samplerate, start_ylim, num_rows, num_cols, data_to_plotter_queue, axis_to_plotter_queue)
-        
+
         self.analysis_thread.start()
 
         while True:
