89c9a3b6889fa8b31af3ac1d309b8057324cf54b
Bernhard Stadlbauer <b.stadlbauer@gmx.net>
2019-03-20 16:21:47 +0100

    fix: Add main line and freeze support

--- start_analyzer.py
+++ start_analyzer.py
@@ -1,8 +1,14 @@
+from multiprocessing import freeze_support
+
+
 import matplotlib
 
 matplotlib.use('Qt5Agg')  # MUST BE CALLED BEFORE IMPORTING matplotlib.pyplot
 
 from analyzer.connect import ConnectorProc
 
-main_process = ConnectorProc()
-main_process.run()
+
+if __name__ == '__main__':
+    freeze_support()
+    main_process = ConnectorProc()
+    main_process.run()
