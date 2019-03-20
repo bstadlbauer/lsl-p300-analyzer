2a9e169269cf7f3f3e2790a922a2dbfdcc25b684
Bernhard Stadlbauer <b.stadlbauer@gmx.net>
2019-03-20 16:21:47 +0100

    fix: Add main line and freeze support

--- start_testserver.py
+++ start_testserver.py
@@ -1,4 +1,9 @@
+from multiprocessing import freeze_support
+
 from testserver.bcni_data_server import BCNIDataServer
 
-server = BCNIDataServer("s1.mat")
-server.start()
+
+if __name__ == '__main__':
+    freeze_support()
+    server = BCNIDataServer("s1.mat")
+    server.start()
