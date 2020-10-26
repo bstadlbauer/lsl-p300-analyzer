58f25411e1e09a9080575b5d9fdb0abe086212d4
Bernhard Stadlbauer <b.stadlbauer@gmx.net>
2020-10-26 15:57:05 +0100

    feat: Automatic download of test data

--- start_testserver.py
+++ start_testserver.py
@@ -4,5 +4,5 @@ from testserver.bcni_data_server import BCNIDataServer
 
 if __name__ == "__main__":
     freeze_support()
-    server = BCNIDataServer("s1.mat")
+    server = BCNIDataServer(1)
     server.start()
---
+++ testserver/constants.py
@@ -0,0 +1,6 @@
+import os
+
+import xdg
+
+BNCI_CACHE_DIR = os.path.join(xdg.XDG_CACHE_HOME, "bstadlbauer", "p300", "testserver", "bnci", "003-2015")
+BNCI_HORIZON_DATA_URL = "http://bnci-horizon-2020.eu/database/data-sets/003-2015/s{subject_number}.mat"
